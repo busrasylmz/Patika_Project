c835b3a24464fa77c807febbd4163a57891c0b41
Busra <soylemezbusrauk@gmail.com>
2022-09-01 21:09:26 +0100

    Ortalama Hesaplama

---
+++ src/Projects/OrtalamaHesaplama.java
@@ -0,0 +1,20 @@
+package Projects;
+
+public class OrtalamaHesaplama {
+
+    public static void main(String[] args) {
+
+        int[] list = {4, 8, 15, 16, 23, 42};
+
+        double sum = 0;
+        for (double i : list) {
+            sum += 1 / i;
+        }
+        double average = list.length / sum;
+        System.out.println("Harmonic Mean\n" + average);
+    }
+
+
+}
+
+
